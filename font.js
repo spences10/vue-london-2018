9d41e21f1bd8fc5a9dbf3d60086788a1649fc8cb
spences10 <spences10apps@gmail.com>
2018-09-24 19:23:17 +0100

    no font

--- font.js
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from 'styled-components'
-
-export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts-for-the-font-god.netlify.com/dank-mono.css?family=dm');
-  body {
-    font-family: dm;
-  }
-`
