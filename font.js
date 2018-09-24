64a96db1ec3db9c60860943977d52c0bba92623c
spences10 <spences10apps@gmail.com>
2018-09-24 12:13:16 +0100

    add font.js

---
+++ font.js
@@ -0,0 +1,8 @@
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  @import url('https://fonts-for-the-font-god.netlify.com/dank-mono.css?family=dm');
+  body {
+    font-family: dm;
+  }
+`
