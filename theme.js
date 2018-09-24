7193b88e7a288bb62a461803da61c327871f8b93
spences10 <spences10apps@gmail.com>
2018-09-24 14:25:21 +0100

    remove font

--- theme.js
+++ theme.js
@@ -1,11 +1,8 @@
 import theme from 'mdx-deck/themes'
-import './font'
 
 export default {
   // extends the default theme
   ...theme,
-  // add a custom font
-  font: 'dm, Roboto, sans-serif',
   // custom colors
   colors: {
     text: '#34434b',
