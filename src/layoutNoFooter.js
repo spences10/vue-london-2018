584ec1646b2ec914434df5e8657e13ee8c9557b2
spences10 <spences10apps@gmail.com>
2018-09-27 14:27:58 +0100

    add layout

--- src/layoutNoFooter.js
+++ src/layoutNoFooter.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Wrapper = styled.main`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: center;
-  position: relative;
-`
+const Wrapper =
+styled.main`width: 100vw; height: 100vh; display: flex; align-items: center; flex-direction: row; justify-content: center; position: relative;`
 
 const LayoutNoFooter = ({ children }) => <Wrapper>{children}</Wrapper>
 
